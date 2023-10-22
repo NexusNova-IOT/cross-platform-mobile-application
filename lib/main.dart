0b5cb44d06199aa796f336e7fb2ad6c03124eb58
EdgarU <u202020371@upc.edu.pe>
2023-10-22 18:56:34 -0500

    feat(profile): added profile widget first version

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:life_travel/profile_management/presentation/profile_management.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Welcome to life-travel!'),
+          child: UserProfile(), // Usa el widget MyContainer aquí
         ),
+
       ),
     );
   }
