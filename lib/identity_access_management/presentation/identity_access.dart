a8bd609d71d6ec968b9eeebe81e08bbb63615bfd
Ryan Sweden <u202011397@upc.edu.pe>
2023-10-24 22:53:51 -0500

    feat(authentication): added login widget

--- lib/identity_access_management/presentation/identity_access.dart
+++ lib/identity_access_management/presentation/identity_access.dart
@@ -1,1 +1,97 @@
+import 'package:flutter/material.dart';
 
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isButtonEnabled = false;
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(50),
+        child: Form(
+          key: _formKey,
+          onChanged: () {},
+          child: Column(
+            children: [
+              // ClipOval(
+              //     child: Image.asset(
+              //   'assets/logo.png',
+              //   height: size.height * 0.25,
+              // )),
+              const SizedBox(height: 50),
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'username',
+                  hintText: 'username',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(_isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                obscureText: !_isPasswordVisible,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => {},
+                    child: const Text('Sign In'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:life_travel/iot_asset_management/presentation/animated_marker_map.dart';
+import 'package:life_travel/identity_access_management/presentation/identity_access.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +14,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: AnimatedMarkerMap(),
+          child: SignInScreen(),
         ),
       ),
     );
