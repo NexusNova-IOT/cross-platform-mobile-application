fc86842d6c1c3bfd43fd9420fdd523b884b98b8c
NicolePT <U201921559@upc.edu.pe>
2023-11-13 12:55:11 -0500

    feat: added file for tour packages repository

---
+++ lib/tour_packages/infraestructure/repositories/tour_package_repository.dart
@@ -0,0 +1,31 @@
+import 'package:life_travel/tour_packages/domain/entities/tour_package.dart';
+
+import '../../domain/interfaces/tour_package_interface.dart';
+import '../data_sources/tour_package_data_provider.dart';
+
+
+class TourPackageRepositoryImpl implements TourPackageRepository {
+  final TourPackageDataProvider dataProvider;
+
+  TourPackageRepositoryImpl({required this.dataProvider});
+
+  @override
+  Future<List<TourPackage>> fetchAllTourPackages() async {
+    try {
+      final List<TourPackage> tourPackages = await dataProvider.fetchTourPackages();
+      return tourPackages;
+    } catch (e) {
+      throw Exception('Failed to fetch tour packages: $e');
+    }
+  }
+
+  @override
+  Future<TourPackage> getPackageById(int id) async {
+    try {
+      final TourPackage tourPackage = await dataProvider.getTourPackage(id);
+      return tourPackage;
+    } catch (e) {
+      throw Exception('Failed to get tour package with ID $id: $e');
+    }
+  }
+}
