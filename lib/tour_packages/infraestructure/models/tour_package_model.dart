193bfff2ce1abf6576b6e2b24b013ccb8f64da0f
DominikMendoza <U20201b980@upc.edu.pe>
2023-11-13 12:56:58 -0500

    fix: modify tour_package model

--- lib/tour_packages/infraestructure/models/tour_package_model.dart
+++ lib/tour_packages/infraestructure/models/tour_package_model.dart
@@ -4,39 +4,47 @@ import 'package:meta/meta.dart';
 class TourPackageModel extends TourPackage {
   TourPackageModel({
     required int id,
-    required int tourPackageId,
-    required DateTime tourExperienceDate,
-    required String tourPackageTitle,
-    required String agencyName,
+    required String title,
+    required String departmentName,
+    required String description,
     required String imgUrl,
+    required int rating,
+    required double meetingPointLatitude,
+    required double meetingPointLongitude,
   }) : super(
           id: id,
-          tourPackageId: tourPackageId,
-          tourExperienceDate: tourExperienceDate,
-          tourPackageTitle: tourPackageTitle,
-          agencyName: agencyName,
+          title: title,
+          departmentName: departmentName,
+          description: description,
           imgUrl: imgUrl,
+          rating: rating,
+          meetingPointLatitude: meetingPointLatitude,
+          meetingPointLongitude: meetingPointLongitude,
         );
 
   factory TourPackageModel.fromJson(Map<String, dynamic> json) {
     return TourPackageModel(
       id: json['id'],
-      tourPackageId: json['tourPackageId'],
-      tourExperienceDate: json['tourExperienceDate'],
-      tourPackageTitle: json['tourPackageTitle'],
-      agencyName: json['agencyName'],
+      title: json['title'],
+      departmentName: json['departmentName'],
+      description: json['description'],
       imgUrl: json['imgUrl'],
+      rating: json['rating'],
+      meetingPointLatitude: json['meetintPointLatitude'],
+      meetingPointLongitude: json['meetintPointLongitude'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'tourPackageId': tourPackageId,
-      'tourExperienceDate': tourExperienceDate,
-      'tourPackageTitle': tourPackageTitle,
-      'agencyName': agencyName,
+      'title': title,
+      'departmentName': departmentName,
+      'description': description,
       'imgUrl': imgUrl,
+      'rating': rating,
+      'meetintPointLatitude': meetingPointLatitude,
+      'meetintPointLongitude': meetingPointLongitude,
     };
   }
 }
