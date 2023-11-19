48726c2ca1e574e0166913830a8a87c58efca18e
RyanSweden03 <U202011397@upc.edu.pe>
2023-11-19 10:19:55 -0500

    chore(style): update Mapbox style

--- lib/common/config/mapbox_credentials.dart
+++ lib/common/config/mapbox_credentials.dart
@@ -2,5 +2,5 @@ class MapBoxCredentials {
   static const MAP_BOX_TOKEN =
       'pk.eyJ1IjoicnlhbnN3ZWRlbiIsImEiOiJjbDI0emlqZ2gwNG42M2lwZzdsM2k1N2w2In0.t8q9bDscpRha_4kXK1kdyg';
 
-  static const MAPBOX_STYLE = 'mapbox/dark-v11';
+  static const MAPBOX_STYLE = 'mapbox/streets-v12';
 }
